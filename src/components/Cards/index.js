99a96f7e18ed640a334e8cc50e0dc3ea98e498ed
Gustavo <valbuena52@gmail.com>
2022-10-29 14:11:29 -0300

    logic to save images to local storage

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,7 +1,32 @@
-import { useState, useEffect } from 'react'
 import './style.css';
 
 function Cards({ images }) {
+
+  // move this to a hook
+  const handleSaveImage = (href, title) => {
+    const imageToSave = {
+      href,
+      title
+    }
+
+    console.log(imageToSave)
+
+    const storedImages = window.localStorage.getItem("saved_images")
+
+    console.log(JSON.parse(storedImages))
+
+    if(!storedImages) {
+        return window.localStorage.setItem("saved_images", JSON.stringify([imageToSave]))
+    }
+    const parsedStoredImages = JSON.parse(storedImages)
+
+    const objectToInsert = [
+        ...parsedStoredImages,
+        imageToSave
+    ]
+    
+    window.localStorage.setItem("saved_images", JSON.stringify(objectToInsert))
+  }
   return (
     <div>
         {images.map( (image, index) => (
@@ -9,7 +34,10 @@ function Cards({ images }) {
             <img src={image.links[0].href} alt={image.data[0].title}/>
             <div>
               <span>{image.data[0].title}</span>
-              <button>Download</button>
+              <button onClick={() => handleSaveImage(
+                image.links[0].href,
+                image.data[0].title
+              )}>Download</button>
             </div>
           </div>
         ))}
